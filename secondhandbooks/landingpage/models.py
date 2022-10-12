ea0efee7e7fb0b3b89f2f5bb5dac2c6b795a60a5
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-10-12 20:04:00 +0000

    new data models books and author

--- secondhandbooks/landingpage/models.py
+++ secondhandbooks/landingpage/models.py
@@ -7,7 +7,8 @@ from cloudinary.models import CloudinaryField
 class Author(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False)
     # image = CloudinaryField('image')
-    image = models.FileField(upload_to ='static/media/uploads/') 
+    image = models.FileField(upload_to ='static/media/uploads/')
+    details = models.CharField(max_length=1024) 
 
     def __str__(self):
         return self.name
@@ -17,7 +18,11 @@ class Books(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     value = models.FloatField(null=False, blank=False)
     # book_cover = CloudinaryField('image')
-    book_cover = models.FileField(upload_to ='static/media/uploads/') 
+    book_cover = models.FileField(upload_to ='static/media/uploads/')
+    synopsis = models.CharField(max_length=2048, null=False , blank=False)
+    details = models.CharField(max_length=2048, null=False , blank=False)
+    amount = models.IntegerField()
+    
 
     def __str__(self):
         return self.name
