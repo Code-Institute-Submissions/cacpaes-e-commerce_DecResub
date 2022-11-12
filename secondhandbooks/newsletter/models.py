0bd43b7058bf59bc0bc4fd652c3b2ac2aa593116
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-12 21:24:15 +0000

    comment to model newsleatter

--- secondhandbooks/newsletter/models.py
+++ secondhandbooks/newsletter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class SubscribedUsers(models.Model):
+    """ Entity to subcribed users to newsleatter"""
     email = models.CharField(unique=True, max_length=50)
     name = models.CharField(max_length=50)
     user = models.ForeignKey(
