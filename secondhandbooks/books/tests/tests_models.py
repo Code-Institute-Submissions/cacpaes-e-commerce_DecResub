a6e9bbba3330217c523e02881088c170e7571589
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-15 15:57:58 +0000

    add tests models Category book

--- secondhandbooks/books/tests/tests_models.py
+++ secondhandbooks/books/tests/tests_models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from books.models import Review, Book
+from books.models import Review, Book, Category, Author
 
 
 
@@ -13,13 +13,14 @@ class ReviewTestCase(TestCase):
 
 
     def setUp(self):
+
+
         """
         Defined function before condition for test
         """
 
 
         user = User.objects.create(username='test', password='test')
-
         user2 = User.objects.create(username='test2', password='test2')
 
         book = Book.objects.create(
@@ -37,7 +38,7 @@ class ReviewTestCase(TestCase):
         )
 
         Review.objects.create(
-            user=user
+            user=user,
             book=book,
             review=4
         )
@@ -71,3 +72,104 @@ class ReviewTestCase(TestCase):
         self.assertEquals(reviews[0].review, 4)
         self.assertEquals(reviews[1].review, 5)
         self.assertGreater(reviews[0].created_on, reviews[1].created_on)
+
+
+"""
+class to test model Categoria
+"""
+
+
+class CategoriaTestCase(TestCase):
+
+
+    def setUp(self):
+
+
+        """
+        Defined function before condition for test
+        """
+
+
+        user = User.objects.create(username='test', password='test')
+
+        category_children = Category.objects.create(
+            name="Children's Books",
+            friendly_name="Children's Books"
+        )
+
+        category_health = Category.objects.create(
+            name="Health",
+            friendly_name="Health"
+        )
+
+        category_food = Category.objects.create(
+            name="Food&Drink",
+            friendly_name="Food&Drink"
+        )
+
+        category_crime = Category.objects.create(
+            name="Crime, Thrillers&Mystery",
+            friendly_name="Crime, Thrillers&Mystery"
+        )
+
+        book_children = Book.objects.create(
+            username=user,
+            nome="Harry Potter",
+            description="is very good book",
+            price=73.54,
+            category=category_children
+        )
+
+        book_health = Book.objects.create(
+            username=user,
+            nome="think and grow rich ",
+            description="is very good book",
+            price=82.54,
+            category=category_health
+        )
+
+        book_food = Book.objects.create(
+            username=user,
+            nome="think and grow rich ",
+            description="is very good book",
+            price=82.54,
+            category=category_food
+        )
+
+        book_crime = Book.objects.create(
+            username=user,
+            nome="think and grow rich ",
+            description="is very good book",
+            price=82.54,
+            category=category_crime
+        )
+
+    def test_cetegory_return_str(self):
+        """
+        Test string for cetegory
+        """
+        category = Category.objects.get(name="Children's Books")
+        self.assertEquals(category.__str__(), "Children's Books")
+
+    def test_confirm_data(self):
+        """
+        Test confirm objects atributes
+        """
+        category = Category.objects.get(name="Health")
+        self.assertEquals(category.name, "Health")
+        self.assertEquals(category.friendly_name, "Health")
+
+    def test_get_book_category(self):
+        """
+        Test return books in filter category
+        """
+        category = Category.objects.get(name="Health")
+        books = Book.objects.get(category=category)
+        self.assertIsNotNone(book)
+
+        Category.objects.delete(category)
+        books = Book.objects.get(category=category)
+        self.assertIsNone(book)        
+
+
+  
