dc5a958d62a79e49dde4bc1a217e9aeef0a056df
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-15 18:58:13 +0000

    add test forms books

--- secondhandbooks/books/tests/tests_forms.py
+++ secondhandbooks/books/tests/tests_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from books.forms import ReviewForm
-
+from books.forms import ReviewForm, BookForm
+from books.models import Category
 
 """
 class to test form ReviewForm
@@ -41,5 +41,65 @@ class ReviewFormTestCase(TestCase):
         """
         form = ReviewForm(data={
             'review': 10,
+        })
+        self.assertFalse(form.is_valid(), form.errors)
+
+
+"""
+class to test form BookForm
+"""
+class BookFormTestCase(TestCase):
+
+    def setUp(self):
+
+        """
+        Create setup book form
+        """
+        self.category = Category.objects.create(
+            name="Children's Books",
+            friendly_name="Children's Books"
+        )
+
+        self.category.save()
+
+    def test_book_form_valid(self):
+        """
+        Check if the form is valid
+        """
+        form = BookForm(data={
+            'name': 'Harry Potter',
+            'description': 'The best book',
+            'price': 70
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_book_form_invalid_skip_value_name(self):
+        """
+        Check if the form is invalid, parameter name blank
+        """
+        form = BookForm(data={
+            'description': 'The best book',
+            'price': 70
+        })
+        self.assertFalse(form.is_valid(), form.errors)
+
+    def test_book_form_invalid_description(self):
+        """
+        Check if the form is invalid, parameter description none
+        """
+        form = BookForm(data={
+            'name': 'Harry Potter',
+            'price': 70
+        })
+        self.assertFalse(form.is_valid(), form.errors)
+
+
+    def test_book_form_invalid_price_none(self):
+        """
+        Check if the form is invalid, parameter price none
+        """
+        form = BookForm(data={
+            'name': 'Harry Potter',
+            'description': 'The best book',
         })
         self.assertFalse(form.is_valid(), form.errors)    
