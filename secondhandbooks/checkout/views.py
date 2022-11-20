f2b36794e5d5f9d4f085d449a7f7a83b8b42797d
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-20 20:27:50 +0000

    checkout user not logged in redirect login page

--- secondhandbooks/checkout/views.py
+++ secondhandbooks/checkout/views.py
@@ -41,130 +41,133 @@ def cache_checkout_data(request):
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    if request.user.is_authenticated:
 
-    if request.method == 'POST':
-        bag = request.session.get('bag', {})
+        if request.method == 'POST':
+            bag = request.session.get('bag', {})
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
-        }
+            form_data = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'phone_number': request.POST['phone_number'],
+                'country': request.POST['country'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'county': request.POST['county'],
+            }
 
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
-            for item_id, item_data in bag.items():
-                try:
-                    book = Book.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        if book.amount < item_data:
-                            raise BookUnavailableBook
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            book=book,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
-                        book.amount = book.amount - item_data
-                        book.save()
-                        
-                        
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            if book.amount < quantity:
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_bag = json.dumps(bag)
+                order.save()
+                for item_id, item_data in bag.items():
+                    try:
+                        book = Book.objects.get(id=item_id)
+                        if isinstance(item_data, int):
+                            if book.amount < item_data:
                                 raise BookUnavailableBook
                             order_line_item = OrderLineItem(
                                 order=order,
                                 book=book,
-                                quantity=quantity,
-                                book_size=size,
+                                quantity=item_data,
                             )
                             order_line_item.save()
-                            book.amount = book.amount - quantity
+                            book.amount = book.amount - item_data
                             book.save()
-                except Book.DoesNotExist:
-                    messages.error(request, (
-                        "One of the books in your bag wasn't "
-                        "found in our database. "
-                        "Please call us for assistance!")
-                    )
-                    order.delete()
-                    return redirect(reverse('view_bag'))
-                except BookUnavailableBook:
-                    messages.error(request, (
-                        """Book is not avaliadbled""", )
-                    )
-                    order.delete()
-                    return redirect(reverse('view_bag'))  
+                            
+                            
+                        else:
+                            for size, quantity in item_data['items_by_size'].items():
+                                if book.amount < quantity:
+                                    raise BookUnavailableBook
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    book=book,
+                                    quantity=quantity,
+                                    book_size=size,
+                                )
+                                order_line_item.save()
+                                book.amount = book.amount - quantity
+                                book.save()
+                    except Book.DoesNotExist:
+                        messages.error(request, (
+                            "One of the books in your bag wasn't "
+                            "found in our database. "
+                            "Please call us for assistance!")
+                        )
+                        order.delete()
+                        return redirect(reverse('view_bag'))
+                    except BookUnavailableBook:
+                        messages.error(request, (
+                            """Book is not avaliadbled""", )
+                        )
+                        order.delete()
+                        return redirect(reverse('view_bag'))  
 
-            # Save the info to the user's profile if all is well
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success',
-                                    args=[order.order_number]))
+                # Save the info to the user's profile if all is well
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(reverse('checkout_success',
+                                        args=[order.order_number]))
+            else:
+                messages.error(request, ('There was an error with your form. '
+                                        'Please double check your information.'))
         else:
-            messages.error(request, ('There was an error with your form. '
-                                     'Please double check your information.'))
-    else:
-        bag = request.session.get('bag', {})
-        if not bag:
-            messages.error(request,
-                           "There's nothing in your bag at the moment")
-            return redirect(reverse('books'))
+            bag = request.session.get('bag', {})
+            if not bag:
+                messages.error(request,
+                            "There's nothing in your bag at the moment")
+                return redirect(reverse('books'))
 
-        current_bag = bag_contents(request)
-        total = current_bag['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+            current_bag = bag_contents(request)
+            total = current_bag['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
 
-        # Attempt to prefill the form with any info
-        # the user maintains in their profile
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'county': profile.default_county,
-                })
-            except UserProfile.DoesNotExist:
+            # Attempt to prefill the form with any info
+            # the user maintains in their profile
+            if request.user.is_authenticated:
+                try:
+                    profile = UserProfile.objects.get(user=request.user)
+                    order_form = OrderForm(initial={
+                        'full_name': profile.user.get_full_name(),
+                        'email': profile.user.email,
+                        'phone_number': profile.default_phone_number,
+                        'country': profile.default_country,
+                        'postcode': profile.default_postcode,
+                        'town_or_city': profile.default_town_or_city,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'county': profile.default_county,
+                    })
+                except UserProfile.DoesNotExist:
+                    order_form = OrderForm()
+            else:
                 order_form = OrderForm()
-        else:
-            order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, ('Stripe public key is missing. '
-                                   'Did you forget to set it in '
-                                   'your environment?'))
+        if not stripe_public_key:
+            messages.warning(request, ('Stripe public key is missing. '
+                                    'Did you forget to set it in '
+                                    'your environment?'))
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
+    else:
+        return redirect(reverse('account_login'))   
 
 
 def checkout_success(request, order_number):
