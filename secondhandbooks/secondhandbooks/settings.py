ccc815d46035c2aa2d1c8382948ff0526f478423
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-20 19:36:27 +0000

    add footer in base.html

--- secondhandbooks/secondhandbooks/settings.py
+++ secondhandbooks/secondhandbooks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in bookion!
 DEBUG = 'DEVELOPEMENT' in os.environ
-DEBUG = True
+# DEBUG = True
 ALLOWED_HOSTS = ['secondhandbookk.herokuapp.com', 'localhost']
 
 
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'storages',
     'comment',
     'newsletter',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
