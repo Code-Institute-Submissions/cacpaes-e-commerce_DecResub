cdf9978b09c2d0385d63029fc378d337c7e26cf5
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-11-27 22:02:18 +0000

    remove comments models book

--- secondhandbooks/books/models.py
+++ secondhandbooks/books/models.py
@@ -50,7 +50,6 @@ class Book(models.Model):
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-#    site_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
