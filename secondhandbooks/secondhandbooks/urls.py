c673c1c6b130a182b7920c198e4ad8bd575ac0fb
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-12-18 14:49:00 +0000

    adjust config

--- secondhandbooks/secondhandbooks/urls.py
+++ secondhandbooks/secondhandbooks/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import  url
 from django.conf.urls.static import static
 from django.conf import settings
 from books.models import Book
+
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
